03c066ff36a7dea618ba5778a1c226dc77182d4e
Elena Pavlova <pavlova.elena.d4@gmail>
2026-09-06 13:51:12 +0300

    Fix duplicate class and move import to top

--- app/main.py
+++ app/main.py
@@ -1,32 +1,4 @@
 from typing import Union
-class Distance:
-
-    def __init__(self, km: float) -> None:
-        self.km = km
-
-    def __str__(self) -> str:
-        return f"Distance: {self.km} kilometers."
-
-    def __repr__(self) -> str:
-        return f"Distance(km={self.km})"
-
-    def __add__(self, other: Union["Distance", float]) -> "Distance":
-        if isinstance(other, Distance):
-            return Distance(self.km + other.km)
-        return Distance(self.km + other)
-
-    def __iadd__(self, other: Union["Distance", float]) -> "Distance":
-        if isinstance(other, Distance):
-            self.km += other.km
-        else:
-            self.km += other
-        return self
-
-    def __mul__(self, other: float) -> "Distance":
-        return Distance(self.km * other)
-
-    from typing import Union
-
 
 class Distance:
     def __init__(self, km: float) -> None:
